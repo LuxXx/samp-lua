721948eeddee1a9aa31e63cda44c4aedfed762e2
LuxXx <pr0v0ziert@gmx.de>
2017-02-24 21:14:28 +0100

    added patterntable and wait for game init

--- src/samp-lua/Game/Game.cpp
+++ src/samp-lua/Game/Game.cpp
@@ -1,8 +1,15 @@
 #include "Game.hpp"
 #include "../../../sol2/sol.hpp"
+#include "../Utils/Windows.hpp"
 
 sol::state lua;
 
 void initGame() {
+
+	// initialize SAMP
+	{
+		while (GetModuleHandleA("samp.dll") == NULL)
+			Sleep(50);
+	}
 
 }
